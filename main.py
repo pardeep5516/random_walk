4a5a5a16348d576302906f772a26168315db53ff
pardeep5516 <43162246+pardeep5516@users.noreply.github.com>
2020-12-30 08:21:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+direction = [0, 90, 180, 270]
+tim.pensize(25)
+tim.speed('fastest')
+
+for _ in range(500):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(direction))
